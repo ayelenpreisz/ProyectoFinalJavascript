c442412a600a4c31ec5c27e39038bbb8c7c17fdc
Maria Preisz <ayelenpreisz@gmail.com>
2024-03-27 08:43:47 -0300

    proyecto final

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function nombre (usuario){
  let resultadoO = 0
 
  function calculadora(num1, num2, operador) {
+    
      if (operador == '+') {
         resultadoO = num1 + num2
         console.log(resultadoO)
@@ -36,13 +37,12 @@ function nombre (usuario){
              return 'No se puede dividir por cero';
          }
      } else {
-         //return 'Operador no válido';
+         return 'Operador no válido';
      }
-     
- }
- console.log(calculadora(5,8,"+"))
-
-
+    
+}
+ 
+ 
  //CALCULAR PRIMEDIO DE NOTAS USANDO ARREGLOS:  
 
  function promedio() {
@@ -55,6 +55,7 @@ function nombre (usuario){
 
         if (nota.toLowerCase() === 'fin') {
             seguirIngresando = false;
+           
         } else {
             // Convierto a numero lo que el usuario ingresa para que sea valido el promedio y lo agrego al arreglo si es valido
             let notaNum = parseFloat(nota);
@@ -70,6 +71,7 @@ function nombre (usuario){
     if (notas.length === 0) {
         return "No hay notas para calcular el promedio.";
     }
+    console.log("Notas ingresadas:", notas);
 
     // Calculo el promedio y lo muestro
     let suma = 0;
@@ -80,8 +82,6 @@ function nombre (usuario){
 
     return promedio;
 }
-
-// Ejemplo de uso
 
 console.log("El promedio de las notas es:", promedio());
 
@@ -160,15 +160,15 @@ fetch(url)
     return response.json();
   })
   .then(data => {
-    // Manejar los datos obtenidos
+   
     
-    // Ejemplo de operación: Filtrar los aires acondicionados con eficiencia energética A
+   
     document.getElementById('filtrarBtn').addEventListener('click', () => {
         const airesEficientes = data.aires_acondicionados.filter(aire => aire.eficiencia_energetica === 'A+');
         mostrarResultados(airesEficientes);
     });
     
-    // Otro ejemplo de operación: Calcular el precio promedio de los aires acondicionados
+   
     document.getElementById('calcularBtn').addEventListener('click', () => {
         const precios = data.aires_acondicionados.map(aire => aire.precio);
         const precioPromedio = precios.reduce((total, precio) => total + precio, 0) / precios.length;
@@ -176,19 +176,19 @@ fetch(url)
     });
   })
   .catch(error => {
-    // Capturar y manejar errores
+    
     console.error('Error al obtener los datos:', error);
   });
 
   function mostrarResultados(resultado) {
     if (Array.isArray(resultado)) {
-        // Si el resultado es un array (por ejemplo, aires eficientes), lo formateamos como una lista
+      
         const lista = resultado.map(aire => {
             return `<li>${aire.modelo} - ${aire.marca} (${aire.eficiencia_energetica}): $${aire.precio}</li>`;
         }).join('');
         document.getElementById('resultados').innerHTML = `<ul>${lista}</ul>`;
     } else {
-        // Si el resultado no es un array, lo mostramos como texto plano
+        
         document.getElementById('resultados').textContent = resultado;
     }
 }
